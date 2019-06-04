acb7fdd0136060b2f15787f7983d49145ffa0322
nvakhoa <51250613+nvakhoa@users.noreply.github.com>
2019-06-04 15:51:04 +0700

    Add files via upload

--- Image2vector.py
+++ Image2vector.py
@@ -2,14 +2,17 @@ import coco
 from cache import cache
 
 
-import os, time, re
+import os, time, re, math
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
+from spacy.lang.en import English
 
+nlp = English()
 
-stopword = '\stopwords_en.txt'
+stopword = 'stopwords_en.txt'
 dir_path = 'data\coco'
+
 
 def set_path_stopword(newpath):
     global stopword
@@ -52,7 +55,7 @@ def load_image(path, size=None):
     return img
 
 
-def show_image(idx, filenames_val, captions_val):
+def show_image(idx, filenames_val, captions_val, cap=True):
 
 
     dir = coco.val_dir
@@ -63,8 +66,9 @@ def show_image(idx, filenames_val, captions_val):
     path = os.path.join(dir, filename)
 
     # Print the captions for this image.
-    for caption in captions:
-        print(caption)
+    if cap:
+        for caption in captions:
+            print(caption)
 
     # Load the image and plot it.
     img = load_image(path)
@@ -72,11 +76,18 @@ def show_image(idx, filenames_val, captions_val):
     plt.show()
 
 
+def lemma_spacy(str_input):
+    doc = nlp(str_input.lower())
+    lemma =' '.join(i.lemma_  for i in doc)
+    return re.sub( "[^\w]", " ",lemma).split()
+
 def _Image2data(captions, stopwords):
     data = dict()
     voc = []
     for caption in captions:
-        listCap = remove_stopwords(re.sub("[^\w]", " ", caption.lower()).split(),stopwords)
+        #listCap = remove_stopwords(re.sub( "[^\w]", " ",caption).split(),stopwords)
+        listCap = remove_stopwords(lemma_spacy(caption),stopwords)
+
         voc.extend(listCap)
         for word in listCap:
             t = data.get(word)
@@ -117,6 +128,24 @@ def _load_vector(filenames, captions):
                 invert[i].append(j)
 
     Vector = [(BoW[i], voc[i], invert[i]) for i in range(number_voc)]
+
+    f=open('BOW.txt','w')
+    cont =''
+    for i in BoW:
+        cont +=  ' '.join(map(str,i)) + '\n'
+    f.write(cont)
+    f.close()
+
+    f=open('voc.txt','w')
+    cont = '\n'.join(map(str,voc))
+    f.write(cont)
+    f.close()
+
+    f=open('invert.txt','w')
+    cont = '\n'.join(map(str,invert))
+    f.write(cont)
+    f.close()
+
     zBoW, zvoc, zinvert = zip(*Vector)
     return zBoW, zvoc, zinvert
 
@@ -126,7 +155,39 @@ def load_vector(filenames, captions):
 
     vectors = cache(cache_path=cache_path,
                     fn=_load_vector,
-                    filenames_val= filenames,
-                    captions_val= captions)
+                    filenames= filenames,
+                    captions= captions)
     return vectors
 
+def _tf_idf(filenames,captions):
+
+    bow, voc, invert = load_vector(filenames, captions)
+    number_file = len(filenames)
+    number_voc = len(voc)
+
+    tf = np.zeros((number_voc, number_file))
+    idf = np.zeros(number_voc)
+
+    for i in range(number_voc):
+        idf[i] = round(1+ math.log(number_file/len(invert[i])), 3)
+        for j in range(number_file):
+            if bow[i][j] != 0:
+                tf[i][j] = round(1 + math.log(bow[i][j]), 3)
+
+    records = [(np.dot(tf[i],idf[i]), idf[i])
+            for i in range(len(idf))]
+
+    weight, _idf = zip(*records)
+
+    return weight, _idf, voc, invert
+
+
+def load_weight(filenames, captions):
+    cache_filename = 'weight.pkl'
+    cache_path = os.path.join(dir_path, cache_filename)
+
+    weight = cache(cache_path= cache_path,
+                   fn=_tf_idf,
+                   filenames=filenames,
+                   captions=captions)
+    return weight
--- main.py
+++ main.py
@@ -5,9 +5,46 @@ import Image2vector as i2v
 import numpy as np
 import re, time
 
-def Text2vector(Text, voc):
-    listText =  re.sub("[^\w]"," ",Text.lower()).split()
 
+def combine(vector, invert):
+	temp = []
+	for index in range(len(vector)):
+		if vector[index] != 0: 
+			temp.extend(invert[index])
+	return list(set(temp))
+
+
+def negate(arr, number_file):
+    temp=[i
+          for i in range(number_file)
+          if i not in arr]
+    return list(temp)
+
+
+def special(str):
+    begin = re.search(r"\"\b", str)
+    end = re.search(r"\"\b", str[:][:][::-1])
+    if  begin != None: begin = begin.end()
+    if end != None:  end = len(str) - end.start()-1
+    return (begin, end)
+
+
+def standardized(Text):
+    listText = re.sub("[^\w]", " ", Text.lower()).split()
+    return listText
+
+
+def vector_intersect(vector, invert):
+    arr = np.arange(1,len(vector),1)
+    for index in range(len(vector)):
+        if vector[index] != 0:
+            arr = np.intersect1d(np.array(arr), np.array(invert[index]))
+    return arr
+
+
+def Text2vector(Text, voc):
+    #listText = standardized(Text)
+    listText = i2v.lemma_spacy(Text)
     n_voc = len(voc)
     bow = np.zeros(n_voc, dtype=np.int)
     for word in range (n_voc):
@@ -20,15 +57,92 @@ def Text2vector(Text, voc):
     return bow
 
 
-def Norm2(Query, BoW, voc):
-    Query = Text2vector(Query,voc)
+def Norm2(Query, BoW, voc, invert):
+    vector = Text2vector(Query,voc)
+    BoW = np.array(BoW).T
+    spe = special(Query)
+    plus = []
+    substring = []
+    vector = Text2vector(Query, voc)
+    if spe[0] != None:
+        plus = vector_intersect(vector, invert)
+
+        #Query = Query[:spe[0]] + Query[spe[1]:]
+
+    plus = vector_intersect(vector, invert)
+
+    m_content = standardized(Query)
+    weight = []
+#    list_nameInverted = []
+#    for word in m_content:
+#        if word in voc:
+#            i = voc.index(word)
+#            w = 1
+#            if word in substring:
+#                w = 0.5
+#            for name in invert[i]:
+#                if name in list_nameInverted:
+#                    indw = list_nameInverted.index(name)
+#                else:
+#                    print(name)
+#                    list_nameInverted.append(name)
+#                    weight.append(w)
+
+    list_nameInverted = list(set(plus))
 
     distance = dict()
-    for i in range(len(BoW)):
-        distance[i] = np.linalg.norm(BoW[i] - Query)
-
+    for i in range(len(list_nameInverted)):
+        distance[list_nameInverted[i]] = np.linalg.norm(BoW[list_nameInverted[i]] - vector)
     dist = sorted(distance, key=distance.get)
+    return  dist
+
+
+def distance_tf_idf(Query, weight, idf, voc):
+    tf_query = Text2vector(Query, voc) * idf
+    vector = np.array(weight).T
+    dist = dict()
+    for index in range(len(vector)):
+        dist[index] =  np.linalg.norm(vector[index] - tf_query)
+    dist = sorted(dist,key=dist.get)
     return dist
+
+
+def distance(Query, bow, voc):
+    bow = np.array(bow).T
+    vt = Text2vector(Query, voc)
+
+    Distance = dict()
+    for i in range(len(bow)):
+        Distance[i] = np.linalg.norm(bow[i] - vt)
+
+    dist = sorted(Distance,key=Distance.get)
+    return dist
+
+
+def consine(vectorQuery, weightT, voc, idf, norm2, ListIDdoc):
+    tic=time.time()
+
+    norm2_query = np.linalg.norm(vectorQuery)
+    if norm2_query == 0:return []
+    consine = dict()
+    for index in range(len(ListIDdoc)):
+        consine[ListIDdoc[index]] = np.dot(weightT[ListIDdoc[index]],vectorQuery)/(norm2[ListIDdoc[index]]* norm2_query)
+    consine = sorted(consine, key=consine.get, reverse=True)
+    # print('consine ', time.time() - tic)
+    return consine
+
+
+def decode_query(Query, weightT, voc, idf, norm2, inverted):
+    spe = special(Query)
+    vector_query = Text2vector(Query, voc)*idf
+    plus = combine(vector_query, inverted)
+    if spe[0] != None and spe[1] != None:
+        vector = Text2vector(Query[spe[0]: spe[1]], voc)
+        plus = vector_intersect(vector, inverted)
+        if Query[spe[0]-2] == '-':
+            plus = negate(plus,len(weightT))
+    return consine(vector_query, weightT, voc, idf, norm2, plus)
+
 
 tic = time.time()
 # path file stopwords_en.txt
@@ -41,18 +155,28 @@ i2v.set_dir_path(dir_path)
 
 
 _, filenames_val, captions_val = coco.load_records(train=False)
-BoW, voc, invert = i2v.load_vector(filenames_val, captions_val)
+#BoW, voc, invert = i2v.load_vector(filenames_val, captions_val)
+#BoW = np.array(BoW).T
 
-BoW = np.array(BoW).T
+
+weight, idf, voc, invert = i2v.load_weight(filenames_val,captions_val)
+
+
+weightT = np.array(weight).T
+norm2 = [np.linalg.norm(i)
+         for i in weightT]
+
 print("time to load: ", time.time() - tic)
 
-tic= time.time()
-Query = 'bear'
-
-idx = Norm2(Query, BoW, voc)
-print("time search : ", time.time() - tic)
-
-
-for i in range(len(idx)):
-    if i > 5: break
-    i2v.show_image(i,filenames_val, captions_val)
+while True:
+    Query = input('Nhap cau hoi: ')
+    if Query == '--exit':break
+    tic= time.time()
+    #idx =#distance(Query,weight, idf, voc, filenames_val)
+    #idx = Norm2(Query, BoW, voc,invert)
+    #idx = distance(Query,BoW, voc)
+    idx = decode_query(Query, weightT, voc, idf, norm2, invert)
+    print('Có ',len(idx)," kết quả (", round(time.time() - tic,2), 's)',sep='')
+    for i in range(len(idx)):
+        if i > 3: break
+        i2v.show_image(idx[i],filenames_val, captions_val, False)
